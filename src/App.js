bd11e91aa7e81726e1122e15e240446762adf2d1
NoelEvan321 <noel_evan@outlook.com>
2023-02-09 22:41:43 -0600

    Added the Routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import './App.css'
+import Navigation from './components/Navigation'
+import Home from './components/Home/Index'
+import Affiliations from './components/Affiliations/Affiliations'
+import BibleSearch from './components/BibleSearch/BibleSearch'
+import DiscoveryMethod from './components/Discovery/DiscoveryMethod'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+export default function App() {
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/affiliations' element={<Affiliations />} />
+          <Route path='/bibleSearch' element={<BibleSearch />} />
+          <Route path='/discoveryMethod' element={<DiscoveryMethod />} />
+        </Routes>
+      </Router>
 }
-
-export default App;
